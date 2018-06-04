49566c9762d374ee88542d5f384305f6187765ef
xdd <767403510@qq.com>
2018-06-04 17:40:17 +0800

    增加zuul网管工程

---
+++ xdd-micro-zuul/src/main/java/com/java/xdd/eureka/filter/TokenFilter.java
@@ -0,0 +1,45 @@
+package com.java.xdd.eureka.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class TokenFilter extends ZuulFilter{
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+
+            }
+            return "fail";
+        }
+        return "ok";
+    }
+}
